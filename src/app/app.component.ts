27d1dc06a8e1a6c9f65e944491c898adbc446dc7
tobiy105 <75105019+tobiy105@users.noreply.github.com>
2024-03-17 18:08:26 +0000

    Help page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,13 @@ import { RouterOutlet } from '@angular/router';
 import { HeroComponent } from './hero/hero.component';
 import { HeaderComponent } from './header/header.component';
 import { FeaturesComponent } from './features/features.component';
+import { HelpComponent } from './help/help.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeroComponent, RouterModule, HeaderComponent, FeaturesComponent, FontAwesomeModule],
+  imports: [RouterOutlet, HeroComponent, RouterModule, HeaderComponent, FeaturesComponent, HelpComponent, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
