861b72408acb2c2ecfac67fafe02c8839f683d4e
Ishmam Ahmed <ishmam.ahmed24@gmail.com>
2024-03-19 22:54:24 +0000

    Navbar w/ scroll

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ElementRef, Renderer2 } from '@angular/core';
 import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -11,4 +11,16 @@ export class HeaderComponent {
   faBars = faBars;
   faTimes = faTimes;
   isMenuOpen = false;
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    const element = this.el.nativeElement.querySelector('nav');
+    if (window.scrollY > 100) {
+      this.renderer.addClass(element, 'scrolled-navbar');
+    } else {
+      this.renderer.removeClass(element, 'scrolled-navbar');
+    }
+  }
 }
