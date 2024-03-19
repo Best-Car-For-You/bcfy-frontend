a3857d27536fb0421f8fd0ab466b53eae2ef6468
tobiy105 <75105019+tobiy105@users.noreply.github.com>
2024-03-19 00:48:40 +0000

    message form validation

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ ReactiveFormsModule, CommonModule ], 
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+  
+  messageForm: FormGroup;
+  isFormSubmitted: boolean = false;
+
+  constructor() {
+    this.messageForm = new FormGroup({
+      name: new FormControl('',[Validators.required, Validators.minLength(3)]),
+      email: new FormControl('',[Validators.required, Validators.email]),
+      message: new FormControl('',[Validators.required, Validators.minLength(10)])
+    });
+  }
+  onSubmit() {
+
+    const isFormValid = this.messageForm.valid;
+    this.isFormSubmitted = true;
+    // Placeholder for submitting the form
+    if (isFormValid) {
+      alert('Your message has been sent successfully!');
+    }
+    
+  }
 
 }
