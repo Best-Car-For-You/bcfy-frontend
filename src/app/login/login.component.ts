7ec7469668f72a9e0719a125b7931fc7b72efce9
tobiy105 <75105019+tobiy105@users.noreply.github.com>
2024-03-20 20:33:27 +0000

    login page design

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,10 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username: string;
+  password: string;
 
+  constructor() {
+    this.username = '';
+    this.password = '';
+  }
+
+  login() {
+    // Perform login logic here
+    console.log('Username:', this.username);
+    console.log('Password:', this.password);
+  }
 }
