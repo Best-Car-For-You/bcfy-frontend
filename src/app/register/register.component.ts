59d8539d668b72aa19bdc99e0505c3f716280057
tobiy105 <75105019+tobiy105@users.noreply.github.com>
2024-04-05 17:54:13 +0100

    NgModule - register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { IUser, CognitoService } from '../cognito.service';
 
 @Component({
   selector: 'app-register',
@@ -6,18 +8,52 @@ import { Component } from '@angular/core';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
+
 export class RegisterComponent {
-  username: string;
-  password: string;
 
-  constructor() {
-    this.username = '';
-    this.password = '';
+  loading: boolean;
+  isConfirm: boolean;
+  user: IUser;
+
+  constructor(private router: Router,
+              private cognitoService: CognitoService) {
+    this.loading = false;
+    this.isConfirm = false;
+    this.user = {} as IUser;
   }
 
-  register() {
-    // Perform register logic here
-    console.log('Username:', this.username);
-    console.log('Password:', this.password);
+  public signUp(): void {
+    this.loading = true;
+    this.cognitoService.signUp(this.user)
+    .then(() => {
+      this.loading = false;
+      this.isConfirm = true;
+    }).catch(() => {
+      this.loading = false;
+    });
   }
+
+  public confirmSignUp(): void {
+    this.loading = true;
+    this.cognitoService.confirmSignUp(this.user)
+    .then(() => {
+      this.router.navigate(['/signIn']);
+    }).catch(() => {
+      this.loading = false;
+    });
+  }
+
+  // username: string;
+  // password: string;
+
+  // constructor() {
+  //   this.username = '';
+  //   this.password = '';
+  // }
+
+  // register() {
+  //   // Perform register logic here
+  //   console.log('Username:', this.username);
+  //   console.log('Password:', this.password);
+  // }
 }
